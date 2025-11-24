94a6c1983d625b444f0a9feb2c1ca7d9e759118f
MaximVis <125341611+MaximVis@users.noreply.github.com>
2025-11-25 02:10:16 +0600

    Delete src directory

--- src/shapka.php
+++
@@ -1,4 +0,0 @@
-    <div class = "container_header"><!-- шапка(лого) -->
-        <div class="image-container"><img src="imgs/logo.jpg"></div>
-        <div class = "logo_word"> allgames </div>
-    </div>
